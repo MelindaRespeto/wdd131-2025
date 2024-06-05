95143a4f2dca8b85cd97d7001644b142f00914df
MelindaRespeto <melindacompetente8@gmail.com>
2024-06-05 21:23:14 +0800

    pull

---
+++ scripts/getdates.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get Current Date</title>
+</head>
+<body>
+    <h1>Get Current Date</h1>
+    <p id="currentDate"></p>
+
+   
+        
+    <script>
+            document.getElementById("currentyear").textContent = new Date().getFullYear();
+        </script>
+   
+</body>
+</html>
