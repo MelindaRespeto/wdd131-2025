d69f5fa9ee903a907d5ef4550b5dcac9f5118509
MelindaRespeto <melindacompetente8@gmail.com>
2024-06-17 18:55:59 +0800

    commit and push

---
+++ scripts/temples.js
@@ -0,0 +1,35 @@
+function updateCurrentYear() {
+    document.getElementById("2024").textContent = new Date().getFullYear();
+}
+
+
+function updateDateTime() {
+    var now = new Date();
+    var dateTimeString = now.toLocaleString('en-US', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        hour12: true 
+    });
+    document.getElementById('datetime').textContent = dateTimeString;
+}
+
+
+updateCurrentYear();
+updateDateTime();
+setInterval(updateDateTime, 1000);
+const hamburgerIcon = document.querySelector('.hamburger-icon');
+const closeIcon = document.querySelector('.close-icon');
+const navLinks = document.querySelector('.nav-links');
+
+hamburgerIcon.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+});
+
+closeIcon.addEventListener('click', () => {
+    navLinks.classList.remove('open');
+});
