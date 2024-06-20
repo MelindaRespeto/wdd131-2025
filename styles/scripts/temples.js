df55aac1dff5c19f8278434a423b4c0c0b9e7074
MelindaRespeto <melindacompetente8@gmail.com>
2024-06-20 12:22:46 +0800

    change

--- styles/scripts/temples.js
+++ styles/scripts/temples.js
@@ -1,27 +1,14 @@
-
-function updateDateTime() {
-    var now = new Date();
-    var dateTimeString = now.toLocaleString('en-US', {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric',
-        second: 'numeric',
-        hour12: true 
-    });
-    document.getElementById('datetime').textContent = dateTimeString;
-}
-
-
-updateCurrentYear();
-updateDateTime();
-setInterval(updateDateTime, 1000);
-const hamburgerIcon = document.querySelector('.hamburger-icon');
-const closeIcon = document.querySelector('.close-icon');
-const navLinks = document.querySelector('.nav-links');
-
+var now = new Date();
+var dateTimeString = now.toLocaleString('en-US', {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  second: 'numeric',
+  hour12: true
+});
 hamburgerIcon.addEventListener('click', () => {
     navLinks.classList.toggle('open');
 });
@@ -29,3 +16,4 @@ hamburgerIcon.addEventListener('click', () => {
 closeIcon.addEventListener('click', () => {
     navLinks.classList.remove('open');
 });
+
