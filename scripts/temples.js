55d4d753f97d92ce6b4a473f76586916b15cb245
MelindaRespeto <melindacompetente8@gmail.com>
2024-06-20 08:31:52 +0800

    commit and push

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,4 +1,13 @@
-<script>
-    console.console.log(" Melinda");
-            html.getElementById("datetime").textContent = new Date().getFullYear();
-        </script>
+<scripts>
+document.getElementById("datetime").textContent = new Date().getFullYear();
+
+function toggleMenu() 
+    if (menu.classList.contains("showMenu")) 
+        menu.classList.remove("showMenu");
+        closeIcon.style.display = "none";
+        menuIcon.style.display = "block";
+     else 
+        menu.classList.add("showMenu");
+        closeIcon.style.display = "block";
+        menuIcon.style.display = "none";
+</scripts>
