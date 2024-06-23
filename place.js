07b40457cab4681a307b566a909959633a2b6b57
MelindaRespeto <melindacompetente8@gmail.com>
2024-06-23 10:46:52 +0800

    commit and push

--- place.js
+++ place.js
@@ -23,3 +23,9 @@ document.addEventListener('DOMContentLoaded', function() {
         console.error('Element with id "currentDate" not found.');
     }
 });
+function calculateWindChill() {
+    var temperature = parseFloat(document.getElementById('temperature').value);
+    var windSpeed = parseFloat(document.getElementById('windSpeed').value);
+    var windChill = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16));
+    document.getElementById('result').innerHTML = "The wind chill is " + windChill.toFixed(2) + " °F";
+}
