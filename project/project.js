79ba289374299f7e8ecd5a22676f8916736559da
MelindaRespeto <melindacompetente8@gmail.com>
2024-07-06 14:36:32 +0800

    add

--- project/project.js
+++ project/project.js
@@ -1,1 +1,14 @@
-document.getElementById("datetime").textContent = new Date().getFullYear();
+<script>
+const currentYear = new Date().getFullYear();
+
+
+document.getElementByclassname("currentyear").textContent = currentYear;
+const currentDate = new Date();
+const formattedDate = currentDate.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+});
+
+document.getElementById("currentDate").textContent = formattedDate;
+</script>
