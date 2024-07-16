d06f44c6e6d9ef1fdb55d52502c7f1d445cbb5d4
MelindaRespeto <melindacompetente8@gmail.com>
2024-07-16 13:01:44 +0800

    add

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,17 +1,17 @@
-<script>
-        // Function to display last modified date
+
+        
         function updateLastModified() {
             const lastModified = document.lastModified;
             document.getElementById('last-modified').textContent = lastModified;
         }
 
-        // Function to display current year
+    
         function updateYear() {
             const year = new Date().getFullYear();
             document.getElementById('current-year').textContent = year;
         }
 
-        // Initial calls to display last modified date and year
+    
         updateLastModified();
         updateYear();
-    </script>
+    
