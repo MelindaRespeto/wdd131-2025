b3655431c7efff04bc29613918c0e81c1728f015
MelindaRespeto <melindacompetente8@gmail.com>
2024-07-16 08:40:25 +0800

    add

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,10 @@
-     
-    <script>
-            document.getElementById("currentyear").textContent = new Date().getFullYear();
-        </script>
+
+const today = new Date();
+
+const shortDate = new Intl.DateTimeFormat("en-US", {
+    dateStyle: "short"
+}).format(today);
+
+console.log(shortDate); // To display the formatted date in the console
+
+
