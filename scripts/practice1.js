3a40e04829003359ed3b650a64cecac292981ee2
MelindaRespeto <melindacompetente8@gmail.com>
2024-09-08 18:15:46 +0800

    add

---
+++ scripts/practice1.js
@@ -0,0 +1,67 @@
+console.log(" Hello + World")
+console.log ("Melinda")
+var element = document.getElementById("elementID");
+if (element) {
+    element.remove(); // Removes the element from the DOM
+}
+let name = "Melinda_Competente";
+console.log("name");
+console.log("Melinda Competente");
+let p1= "Jomel"
+let p2= "Jonas"
+let drink= "milk"
+let pronoun = "he"
+console.log( p1+
+     ", the friendly neighbor, waved at " +  
+       p2  +  " as "+ 
+       pronoun  + 
+           " walked by "+
+       p2 + "'s house, "  + 
+       p2 +  " smiled back and invited in" 
+       + p1 + " for a cup of "  +
+       drink +".")
+ let num1 = "12";
+ let num2 = 2;
+ console.log(parseInt(num1) + (num2));
+ let x = 5;
+ let y = 10;
+    console.log((x+y)/x);
+ let word = "Competente  ";
+    console.log(word.length-2);
+ let word1 = "Melinda";
+    console.log(word1.length);
+    console.log(word1.toUpperCase());
+ let age =20;
+ if (age >= 18){
+       console.log ("legal age");
+       console.log("You can vote"); }
+    else{
+       console.log("Minor age");
+    } 
+ 
+       console.log ("Thank you!");
+ let grade = 77;
+     if (grade> 100){
+         console.log("Invalid Grade");}                                                             
+     else if(grade >= 98){
+         console.log("With Highest Honor");}
+     else if(grade >= 95){
+            console.log("With High Honor");}  
+     else if (grade>= 90){
+         console.log("With honors");}
+     else if (grade>=75){
+         console.log("Passed");}
+     else if(grade< 75){
+         console.log("Failed");
+     } 
+
+console.log("Logical operators:");   
+console.log(" 1. AND operator- both conditions should be true");         
+      let year  = 18;
+      let Experience = 3;
+      console.log (year >= 18 && experience > 1);
+console.log("2. EITHER conditions should be true");       
+
+let hasDegree = true;
+let experience = 3;
+
