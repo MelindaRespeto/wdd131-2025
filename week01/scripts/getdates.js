18cc8a2230826931aae448d82fab8f07b37eb64d
MelindaRespeto <145196890+MelindaRespeto@users.noreply.github.com>
2025-10-27 13:50:50 +0800

    add

--- week01/scripts/getdates.js
+++ week01/scripts/getdates.js
@@ -2,8 +2,7 @@
 const currentYear = new Date().getFullYear();
 
 // Display the current year in the first paragraph
-document.getElementById("copyright").textContent = `© ${currentYear}
-`;
+document.getElementById("copyright").textContent = `© ${currentYear}`;
 
 // Display the date the document was last modified in the second paragraph
 document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
