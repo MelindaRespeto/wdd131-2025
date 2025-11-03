ea74bb86829b87451f66f62155dba8a742de3e95
MelindaRespeto <145196890+MelindaRespeto@users.noreply.github.com>
2025-11-03 14:13:34 +0800

    change

---
+++ getdates.js
@@ -0,0 +1,13 @@
+function updateLastModified() {
+  const lastModified = document.lastModified;
+  document.getElementById('last-modified').textContent = lastModified;
+}
+
+function updateYear() {
+  const year = new Date().getFullYear();
+  document.getElementById('current-year').textContent = year;
+}
+
+
+updateLastModified();
+updateYear();
